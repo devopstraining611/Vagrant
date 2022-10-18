75b99dd12486c9be1e57101d5ce40cd1945ec239
devopstraining611 <devopstraining611@gmail.com>
2022-10-18 08:13:20 +0530

    all file for HAProxy added

--- HAProxy/web01/web01_bootstrap.sh
+++ HAProxy/web01/web01_bootstrap.sh
@@ -7,11 +7,11 @@ apt-get update -y
 apt-get install apache2 -y
 
 #start and enable Apache web-server
-systemctl start apache2
-systemctl enable apache2
+service apache2 start
+service apache2 enable
 
 #copy the index.html file from sync directory to /var/www/html/ directory
 cp /tmp/index_web01.html /var/www/html/index.html
 
 #reload Apache web-server
-systemctl reload apache2
+service apache2 restart
